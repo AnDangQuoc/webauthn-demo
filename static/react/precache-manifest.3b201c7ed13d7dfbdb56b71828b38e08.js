c0a550a406f406307d6d424a00866f6008826c22
Andq <andq@blockpass.org>
2020-08-12 11:18:41 +0700

    update to use react build

---
+++ static/react/precache-manifest.3b201c7ed13d7dfbdb56b71828b38e08.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c92fbfb64acb9b6ce1c690ca6183b594",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2806b7157ae3e9ce3619",
+    "url": "/static/css/2.ed7b5974.chunk.css"
+  },
+  {
+    "revision": "a1bfcd3aac3eac068ed0",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "2806b7157ae3e9ce3619",
+    "url": "/static/js/2.c98130a2.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.c98130a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1bfcd3aac3eac068ed0",
+    "url": "/static/js/main.6fbffd4c.chunk.js"
+  },
+  {
+    "revision": "ddd0398bb3ac6e4ae2c0",
+    "url": "/static/js/runtime-main.0de188ca.js"
+  }
+]);
