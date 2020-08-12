5b3eed3f5a86b4f794215971708523c9932e8e6a
Andq <andq@blockpass.org>
2020-08-12 15:22:09 +0700

    fix wrong request body

--- static/react/precache-manifest.eaec702966d413f8155fde9dc75983a4.js
+++ static/react/precache-manifest.eaec702966d413f8155fde9dc75983a4.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ec8e8406ff4fb64d184d343225df0f42",
+    "revision": "0eed4247d0f591df549609974f55ecd2",
     "url": "/index.html"
   },
   {
-    "revision": "3bbc068d65eb1806bfb7",
+    "revision": "ce44eae8bbfa4e68cfbe",
     "url": "/static/css/2.9e5dd6a8.chunk.css"
   },
   {
-    "revision": "fb215c48d9a17dba859e",
+    "revision": "3bb817452af9f1e5fccf",
     "url": "/static/css/main.5ecd60fb.chunk.css"
   },
   {
-    "revision": "3bbc068d65eb1806bfb7",
-    "url": "/static/js/2.6c9c8af3.chunk.js"
+    "revision": "ce44eae8bbfa4e68cfbe",
+    "url": "/static/js/2.513b14f4.chunk.js"
   },
   {
     "revision": "3b99ef8cf73f2faa2eae50c0c6928584",
-    "url": "/static/js/2.6c9c8af3.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.513b14f4.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "fb215c48d9a17dba859e",
-    "url": "/static/js/main.4cecda98.chunk.js"
+    "revision": "3bb817452af9f1e5fccf",
+    "url": "/static/js/main.0af85809.chunk.js"
   },
   {
     "revision": "ddd0398bb3ac6e4ae2c0",
