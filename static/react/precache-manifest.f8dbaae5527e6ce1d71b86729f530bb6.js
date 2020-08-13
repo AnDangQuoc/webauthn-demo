1a7b53f944e4f62fd89a13787c37b5acb2877d36
Andq <andq@blockpass.org>
2020-08-13 11:03:47 +0700

    update code

---
+++ static/react/precache-manifest.f8dbaae5527e6ce1d71b86729f530bb6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b561d8cfd07e3f210a9547202b147c1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c7f4b9731069a61a51a9",
+    "url": "/static/css/2.9e5dd6a8.chunk.css"
+  },
+  {
+    "revision": "81e62d44092dbdfcd2b0",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "c7f4b9731069a61a51a9",
+    "url": "/static/js/2.50fc5e0c.chunk.js"
+  },
+  {
+    "revision": "5f1b4ef2ec91b49f96cded4ccece022d",
+    "url": "/static/js/2.50fc5e0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81e62d44092dbdfcd2b0",
+    "url": "/static/js/main.c3f3290f.chunk.js"
+  },
+  {
+    "revision": "ddd0398bb3ac6e4ae2c0",
+    "url": "/static/js/runtime-main.0de188ca.js"
+  },
+  {
+    "revision": "2b9b4872cd25494093c1eb14f0264a0b",
+    "url": "/static/media/jsoneditor-icons.2b9b4872.svg"
+  }
+]);
--- static/react/service-worker.js
+++ static/react/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1b93b645681da51f33bfb685508f52b6.js"
+  "/precache-manifest.f8dbaae5527e6ce1d71b86729f530bb6.js"
 );
 
 self.addEventListener('message', (event) => {
